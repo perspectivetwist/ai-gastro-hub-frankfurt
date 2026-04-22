d34ffe5feb723033dbf4daa94667a9fec9f44a39
Max Geißinger <maxgeissinger@gmail.com>
2026-04-22 16:00:54 +0200

    fix: add basePath /staedte/frankfurt for asset loading via rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: '/staedte/frankfurt',
+  assetPrefix: '/staedte/frankfurt',
+};
 
 export default nextConfig;
